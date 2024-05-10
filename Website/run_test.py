a0c2c08881dcf7da2f3c94e9a5e69e6d22ab5dd1
Maximilianna <zxpazj@126.com>
2024-05-10 12:12:28 +0800

    2024_5_10更新

---
+++ Website/run_test.py
@@ -0,0 +1,17 @@
+import unittest
+import time
+from HTMLTestRunner import HTMLTestRunner
+
+report_dir = 'test_report'
+test_dir = 'test_case'
+
+print("start run test case")
+discover = unittest.defaultTestLoader.discover(test_dir, "add_Commodity_Test.py")
+row = time.strftime("%Y-%m-%d-%H-%M-%S")
+report_name = report_dir + '/' + row + "result.html"
+print("start write report..")
+with open(report_name,"wb") as f:
+    runner = HTMLTestRunner(stream=f,title="test_report",description="erp test")
+    runner.run(discover)
+    f.close()
+print("test end")
