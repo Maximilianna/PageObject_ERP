13eecea3138925b304ba6f5208baf0049a2763b6
Maximilianna <zxpazj@126.com>
2024-05-11 09:54:33 +0800

    2024_5_11更新

--- Website/test_case/add_Brand_Test.py
+++ Website/test_case/add_Brand_Test.py
@@ -7,7 +7,7 @@ from time import sleep
 class add_Brand(TestMyUnit):
     def test_add_Brand(self):
         brand_page = BrandPage(self.driver)
-        Login(self.driver,"XTGLY", "123456")
+        Login(self.driver, "XTGLY", "123456")
         sleep(5)
         brand_page.click_Brand()
         sleep(3)
