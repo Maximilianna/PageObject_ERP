b3b23621dc9b35d6ace22f341e6adffcbf02ea04
Maximilianna <zxpazj@126.com>
2024-05-13 16:06:59 +0800

    2024_5_13更新

--- Driver/driver.py
+++ Driver/driver.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 def get_driver():
-    driver = webdriver.Chrome()
+    # 需要将chromedriver.exe文件放在指定的路径
+    service = Service(r"D:\chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
     return driver
